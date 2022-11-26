3c3990f96bee7a901571dfbf8a7c35a148426977
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-26 18:25:27 -0300

    Created Products table propeties

--- Models/Products.cs
+++ Models/Products.cs
@@ -2,6 +2,11 @@ namespace HospitalWarehouse.Models
 {
     public class Products
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Value { get; set; }
+        public Category ProductCategory { get; set; }
 
     }
 }
