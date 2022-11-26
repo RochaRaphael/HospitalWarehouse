e490cad74d132bbf244d0c41872f4777e786849c
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-26 18:24:29 -0300

    Created ConsumedProducts table propeties

--- Models/ConsumedProducts.cs
+++ Models/ConsumedProducts.cs
@@ -2,6 +2,11 @@ namespace HospitalWarehouse.Models
 {
     public class ConsumedProducts
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Value { get; set; }
+        public Category ProductCategory { get; set; }
+        public Sector SectorThatConsumed { get; set; }
     }
 }
