eeba7fe578803bd5c5ff05a3f9e4e63898a7a252
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-29 12:04:13 -0300

    Mapping propeties in Sector table

--- Data/Mapping/SectorMap.cs
+++ Data/Mapping/SectorMap.cs
@@ -8,6 +8,18 @@ namespace HospitalWarehouse.Data.Mapping
         public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Sector> builder)
         {
             builder.ToTable("Sector");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+               .ValueGeneratedOnAdd()
+               .UseIdentityColumn();
+
+            builder.Property(x => x.SectorName)
+                .IsRequired()
+                .HasColumnName("Sector Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(40);
         }
     }
 }
