65e3934ffeed83488fd471db1dcdfd1b8318db8c
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-29 11:58:44 -0300

    Mapping propeties in ConsumedProducts table

--- Data/Mapping/ConsumedProductsMap.cs
+++ Data/Mapping/ConsumedProductsMap.cs
@@ -8,6 +8,30 @@ namespace HospitalWarehouse.Data.Mapping
         public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<ConsumedProducts> builder)
         {
             builder.ToTable("Consumed Products");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Quantity)
+                .IsRequired()
+                .HasColumnName("Quantity")
+                .HasColumnType("INT");
+
+            builder.Property(x => x.Value)
+                .IsRequired()
+                .HasColumnName("Value")
+                .HasColumnType("MONEY");
+
+
         }
     }
 }
