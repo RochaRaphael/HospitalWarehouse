9692006632dbb5b9ba60577f23a7ae415e6f6b70
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-29 11:44:34 -0300

    Mapping propeties in category table

--- Data/Mapping/CategoryMap.cs
+++ Data/Mapping/CategoryMap.cs
@@ -8,6 +8,18 @@ namespace HospitalWarehouse.Data.Mapping
         public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Category> builder)
         {
             builder.ToTable("Category");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+               .ValueGeneratedOnAdd()
+               .UseIdentityColumn();
+
+            builder.Property(x => x.CategoryName)
+                .IsRequired()
+                .HasColumnName("Category Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(40);
         }
     }
 }
