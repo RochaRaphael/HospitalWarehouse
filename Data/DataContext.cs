c02f04cfd3c1fe8b5244c2683f41999a1b054691
Raphael Rocha <rocharaphael0911@outlook.com>
2022-11-30 20:26:44 -0300

    Migrations added

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,17 +1,26 @@
+using HospitalWarehouse.Data.Mapping;
 using HospitalWarehouse.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace Blog.DataContext
+namespace HospitalWarehouse.DataContext
 {
-    public class BlogDataContext : DbContext
+    public class HospitalDataContext : DbContext
     {
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<ConsumedProducts> Posts { get; set; }
-        public DbSet<Products> Users { get; set; }
-        public DbSet<Sector> UserRoles { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<ConsumedProducts> ConsumedProducts { get; set; }
+        public DbSet<Products> Products { get; set; }
+        public DbSet<Sector> Sector { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlServer("Server=localhost,1433;Database=HospitalWarehouse;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new ConsumedProductsMap());
+            modelBuilder.ApplyConfiguration(new ProductsMap());
+            modelBuilder.ApplyConfiguration(new SectorMap());
+        }
 
     }
 }
---
+++ Migrations/20221130232118_HospitalWarehouse.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalWarehouse.Migrations
+{
+    /// <inheritdoc />
+    public partial class HospitalWarehouse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(name: "Category Name", type: "NVARCHAR(40)", maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sector",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SectorName = table.Column<string>(name: "Sector Name", type: "NVARCHAR(40)", maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sector", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "NVARCHAR(80)", maxLength: 80, nullable: false),
+                    Quantity = table.Column<int>(type: "INT", nullable: false),
+                    Value = table.Column<decimal>(type: "MONEY", nullable: false),
+                    ProductCategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Product_Category",
+                        column: x => x.ProductCategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consumed Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "NVARCHAR(80)", maxLength: 80, nullable: false),
+                    Quantity = table.Column<int>(type: "INT", nullable: false),
+                    Value = table.Column<decimal>(type: "MONEY", nullable: false),
+                    ProductCategoryId = table.Column<int>(type: "int", nullable: false),
+                    SectorThatConsumedId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consumed Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consumed Products_Category_ProductCategoryId",
+                        column: x => x.ProductCategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ConsumedProduct_Sector",
+                        column: x => x.SectorThatConsumedId,
+                        principalTable: "Sector",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consumed Products_ProductCategoryId",
+                table: "Consumed Products",
+                column: "ProductCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consumed Products_SectorThatConsumedId",
+                table: "Consumed Products",
+                column: "SectorThatConsumedId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductCategoryId",
+                table: "Products",
+                column: "ProductCategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consumed Products");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Sector");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
